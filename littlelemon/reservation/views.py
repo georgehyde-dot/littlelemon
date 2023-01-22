1e11f173619a57c30ad3dd66211c5a0ca0b18f93
georgehyde-dot <georgewhyde3@gmail.com>
2023-01-22 10:25:59 -0500

    updated settings

--- littlelemon/reservation/views.py
+++ littlelemon/reservation/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sayHello(request):
- return HttpResponse('Hello World')
+    return HttpResponse('Hello World')
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib import admin 
+from django.urls import path 
+from .views import sayHello 
+  
+urlpatterns = [ 
+    path('', sayHello, name='sayHello'), 
+]
